dc30acc9d545015156331bf9de7670804cd76ded
murata366 <r5.yori123@gmail.com>
2025-09-13 15:17:02 +0900

    設定がメインのルート設定

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,14 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SettingsController;
 
+Route::middleware('auth')->group(function () {
+    Route::get('/settings', [SettingsController::class, 'index'])->name('settings.index');
+    Route::get('/settings/users/create', [SettingsController::class, 'createUser'])->name('settings.createUser');
+    Route::post('/settings/users', [SettingsController::class, 'storeUser'])->name('settings.storeUser');
+});
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.attempt');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
