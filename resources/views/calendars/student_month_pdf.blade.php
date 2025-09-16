57eef8c887125c174710dcf9f041dc93a1017d42
ace366 <ace84111@gmail.com>
2025-09-16 19:27:01 +0900

    カレンダー修正

--- resources/views/calendars/student_month_pdf.blade.php
+++ resources/views/calendars/student_month_pdf.blade.php
@@ -58,17 +58,90 @@
 </head>
 <body>
 
-    <!-- ヘッダ -->
-    <table class="header">
-        <tr>
-            <td class="title">{{ $year }}年 {{ $month }}月 月間カレンダー</td>
-            <td class="meta">
-                生徒：{{ $student->last_name }} {{ $student->first_name }}
-                 ／ 学校：{{ $student->school?->name ?? '未設定' }}
-                 ／ 学年：{{ $student->grade ?? '未設定' }}
-            </td>
-        </tr>
-    </table>
+<!-- ヘッダ -->
+<table class="header" style="width:100%; border-collapse:collapse; margin-bottom:8px; font-size:11px; border:1px solid #999; height:28px;">
+    <tr>
+       
+        {{-- 左側：タイトル --}}
+        <td colspan="9" style="background:#888; color:#fff; font-size:30px; font-weight:700; padding:6px 10px;">
+            {{ $month }} 月分 カレンダー
+        </td>
+
+        {{-- 右側：チェック欄（白背景の表） --}}
+        <td colspan="5" style="padding:0;">
+            <table style="width:100%; border-collapse:collapse; font-size:11px; text-align:center;">
+                <tr>
+                    <td colspan="1" style="border:1px solid #999; padding:10px 4px; background:#fff;">通常</td>
+                    <td colspan="2" style="border:1px solid #999; padding:10px 4px; background:#fff; border-right:none;">月</td>
+                    <td colspan="1" style="border:1px solid #999; padding:10px 4px; background:#fff; border-left:none;">&nbsp;&nbsp;回</td>
+                    <td colspan="1" style="border:1px solid #999; padding:10px 4px; background:#fff;">無料コマ</td>
+                    <td colspan="1" style="border:1px solid #999; padding:10px 4px; background:#fff; border-right:none;"></td>
+                    <td colspan="2" style="border:1px solid #999; padding:10px 4px; background:#fff; border-left:none;">&nbsp;&nbsp;回</td>
+                </tr>
+                <tr>
+                    <td colspan="1" style="border:1px solid #999; padding:10px 4px; background:#fff;">スタ講残</td>
+                    <td colspan="2" style="border:1px solid #999; padding:10px 4px; background:#fff; border-right:none;"></td>
+                    <td colspan="1" style="border:1px solid #999; padding:10px 4px; background:#fff; border-left:none;">&nbsp;&nbsp;回</td>
+                    <td colspan="1" style="border:1px solid #999; padding:10px 4px; background:#fff;">預かり授業残</td>
+                    <td colspan="1" style="border:1px solid #999; padding:10px 4px; background:#fff; border-right:none;"></td>
+                    <td colspan="2" style="border:1px solid #999; padding:10px 4px; background:#fff; border-left:none;">&nbsp;&nbsp;回</td>
+                </tr>
+                <tr>
+                    <td colspan="1" style="border:1px solid #999; padding:10px 4px; background:#fff;">季節講習残</td>
+                    <td colspan="2" style="border:1px solid #999; padding:10px 4px; background:#fff; border-right:none;"></td>
+                    <td colspan="1" style="border:1px solid #999; padding:10px 4px; background:#fff;border-left:none;">&nbsp;&nbsp;回</td>
+                    <td colspan="1" style="border:1px solid #999; padding:10px 4px; background:#fff;"></td>
+                    <td colspan="1" style="border:1px solid #999; padding:10px 4px; background:#fff; border-right:none;">月</td>
+                    <td colspan="2" style="border:1px solid #999; padding:10px 4px; background:#fff; border-left:none;">&nbsp;&nbsp;回</td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+
+{{-- 生徒情報 --}}
+<tr style="background:#ddd; font-weight:700;">
+    <td colspan="7" style="border:1px solid #999; padding:10px 4px;">生徒名</td>
+    <td colspan="5" style="border:1px solid #999; padding:10px 4px;">学校</td>
+    <td colspan="2" style="border:1px solid #999; padding:10px 4px;">学年</td>
+</tr>
+<tr>
+    <td colspan="7" style="border:1px solid #999; padding:10px 6px;">
+        {{ $student->last_name }} {{ $student->first_name }}
+    </td>
+    <td colspan="5" style="border:1px solid #999; padding:10px 6px;">
+        {{ $student->school?->name ?? '未設定' }}
+    </td>
+    <td colspan="2" style="border:1px solid #999; padding:10px 6px;">
+        {{ $student->grade ?? '未設定' }}
+    </td>
+</tr>
+
+{{-- 講師情報 --}}
+<tr style="background:#ddd; font-weight:700; text-align:center;">
+    <td colspan="9" style="border:1px solid #999; text-align:center; vertical-align:middle; padding:10px 4px;">
+        講師情報
+    </td>
+    <td colspan="2" style="border:1px solid #999; padding:10px 4px;">時間数</td>
+    <td style="border:1px solid #999; padding:10px 4px;">前月末過不足</td>
+    <td style="border:1px solid #999; padding:10px 4px;">今月規定回数</td>
+    <td style="border:1px solid #999; padding:10px 4px;">今月実施予定</td>
+</tr>
+
+<tr>
+    <td colspan="2" style="border:1px solid #999; padding:10px 10px;">講師名</td>
+    <td colspan="4" style="border:1px solid #999; padding:10px 4px;"></td>
+    <td colspan="3" style="border:1px solid #999; padding:10px 10px;">教科:</td>
+    <td colspan="2" style="border:1px solid #999; text-align:center; padding:10px 4px;">
+        1回&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;分
+    </td>
+    <td style="border:1px solid #999; text-align:right; padding:10px 4px;">回</td>
+    <td style="border:1px solid #999; text-align:right; padding:10px 4px;">回</td>
+    <td style="border:1px solid #999; text-align:right; padding:10px 4px;">回</td>
+</tr>
+
+</table>
+
+
 
     <!-- カレンダー -->
     <table class="calendar">
